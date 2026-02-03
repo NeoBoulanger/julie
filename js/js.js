bf12d8ec7b6948191f3d9a602c2520727ec5207e
Néo BOULANGER <neo@MacBook-Air-de-Neo.local>
2026-02-03 15:39:59 +0100

    fini

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 //je récupère le bouton
 
-let bouton=document.querySelector('button');
+let bouton=document.querySelector('.oui');
 
 function move(){
     bouton.style.marginLeft=Math.round(Math.random()*70+10)+"%";
@@ -9,6 +9,18 @@ function move(){
 
 bouton.addEventListener('mouseover', move);
 
+let compteur=10;
 bouton.addEventListener('mousedown', (e)=>{
-    alert('Merci');
+
+    if(compteur===1){
+        alert('Bravooooo ! ');
+    }
+    document.querySelector('#merci').classList.remove('hidden');
+    compteur-=1;
+    document.querySelector('#merci h1').textContent="Clique encore " + compteur + " fois !";
+    move();
 });
+
+document.querySelector('.non').addEventListener('click', (e)=>{
+    document.querySelector('body').style.backgroundColor='rgba('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+0.4+')';
+})
